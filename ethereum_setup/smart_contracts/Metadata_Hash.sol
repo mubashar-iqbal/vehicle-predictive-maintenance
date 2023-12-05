44c131805798b3047d7102e3d4676944b11dd5a6
mubashar-iqbal <miqbal.ffb@gmail.com>
2023-12-05 16:15:19 +0200

    basic structure of smart contracts

---
+++ ethereum_setup/smart_contracts/Metadata_Hash.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0;
+
+/**
+* This smart contract store the metadata hash in blockchain.
+*/
+contract Sensor_Configs {
+
+     /**
+     * Variable to define who can deploy the contract.
+     */
+    address owner;  // address indicates variable type, in this case, contract creator account address.
+
+    /**
+     * Constructor code is only run when the contract is created.
+     */
+    constructor() {
+         // The msg variable is a special global variable containing properties allowing blockchain access.
+        owner = msg.sender; //msg.sender indicates the contract creator (contract owner)
+    }
+
+    /**
+     * Modifier to ensure only the admin can execute certain functions.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _; // ; indicates it is a modifier function
+    }
+
+}
